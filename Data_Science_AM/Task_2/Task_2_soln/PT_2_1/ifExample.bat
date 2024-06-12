210972f7b1f569c15968e84e7333dd81ab1a65f8
Smart2881 <164088971+Smart2881@users.noreply.github.com>
2024-06-12 17:04:24 +0100

    Delete Data_Science_AM directory

--- Data_Science_AM/Task_2/Task_2_soln/PT_2_1/ifExample.bat
+++
@@ -1,6 +0,0 @@
-
-
-REM This file uses 'if' and 'else' statement to create new folder
-
-IF EXIST new_folder (mkdir if_folder)
-IF EXIST if_folder (mkdir hyperionDev) ELSE mkdir new-projectsPython formatting provider
